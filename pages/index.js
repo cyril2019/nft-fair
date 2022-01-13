f63510e629323435789c9c8f5bbe846e3bdafbd2
starwiz-7 <aryan2019@iiitkottayam.ac.in>
2022-01-13 11:59:50 +0530

    Added Chakra UI, prettier for formatting

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import { Button, ButtonSpinner } from '@chakra-ui/button';
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="max-w-full min-h-screen bg-green-800">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>
+        <ButtonSpinner></ButtonSpinner>
+      </Button>
+    </div>
+  );
 }

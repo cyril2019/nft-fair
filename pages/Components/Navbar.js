032c2e60fcd4bf627355466a1238e95a4679f19a
Cyril Kunjumon <cyril 2019@iiitkottayam.ac.in>
2022-01-16 19:15:59 +0530

    added custom color pallet

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -4,35 +4,35 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className=" px-5 py-3 sm:px-10 sm:py-5 bg-black text-white">
+    <div className=" px-5 py-3 sm:px-10 sm:py-5 bg-faded-black text-white ">
       {/* Full navbar  */}
       <div className="w-full md:flex justify-between items-center space-x-2 hidden ">
         {/* logo comes here */}
-        <div className="w-10 h-10 bg-purple-600"></div>
+        <div className="w-10 h-10 bg-purple"></div>
         {/* menu */}
-        <ul className="flex justify-between space-x-4 text-gray-300 ">
+        <ul className="flex justify-between space-x-4 text-light-gray">
           <li className="hover:text-white cursor-pointer">Explore</li>
           <li className="hover:text-white cursor-pointer">How it works</li>
           <li className="hover:text-white cursor-pointer">Community</li>
         </ul>
         {/* search  */}
-        <div className="flex items-center bg-purple-500 rounded-md">
+        <div className="flex items-center bg-purple rounded-md">
           <input placeholder="Search" className=" py-1 px-2 text-black rounded-l-md" />
           <AiOutlineSearch className="text-3xl  p-1" />
         </div>
         {/* buttons  */}
         <div className="space-x-2">
-          <button className="border-2 border-solid border-purple-500 px-2 py-1 rounded-md font-bold bg-purple-500 hover:bg-black">
+          <button className="border-2 border-solid border-purple px-2 py-1 rounded-md font-bold bg-purple hover:bg-black">
             Create
           </button>
-          <button className="border-2 border-solid px-2 py-1 rounded-md font-bold hover:bg-white hover:text-purple-500">
+          <button className="border-2 border-solid px-2 py-1 rounded-md  font-bold hover:bg-white hover:text-purple">
             Connect
           </button>
         </div>
       </div>
       {/* Mobile view navbar */}
       <div className="md:hidden flex justify-between items-center ">
-        <div className="w-10 h-10 bg-purple-600"></div>
+        <div className="w-10 h-10 bg-purple"></div>
         <div className="flex space-x-5 text-3xl">
           <AiOutlineSearch />
           <AiOutlineMenu />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'faded-black':'#07070C',
+      'light-purple':'#B854FF',
+      'dark-purple':'#765FFA',
+      'purple':'#915BFF',
+      'white':'#ffffff',
+      'light-gray':'#d1d1d1'
+    },
     screens: {
       'md': '850px',
     },

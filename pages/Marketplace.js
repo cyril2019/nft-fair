152c3868eb5981dab470b7a6cb7490f37de54bb4
charles-vj <charles2019@iiitkottayam.ac.in>
2022-01-24 03:28:35 +0530

    marketplace listing fetch functionality

--- pages/Marketplace.js
+++ pages/Marketplace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Footer from './Components/Footer';
 import ItemTile from './Components/ItemTile';
 import ItemTileList from './Components/ItemTileList';
@@ -6,13 +6,29 @@ import Navbar from './Components/Navbar';
 import { Sidebar, sidebar } from './Components/Sidebar';
 
 const marketplace = () => {
+
+  const [nfts, setNfts] = useState([])
+
+    const getListings = async () => {
+      const listing = await fetch("/api/marketplace", {
+        method: "GET",
+      });
+      const data = await listing.json()
+      setNfts(data);
+      console.log(nfts)
+    }
+
   return (
     <div className="w-full h-full bg-black">
       <Navbar />
       {/* marketplace body  */}
+        <button onClick={getListings}>CALL</button>
       <div className="w-full h-full  flex px-5 py-2 sm:px-10 sm:py-5 ">
         {/* market  */}
-        <ItemTileList />
+        {nfts.map((nft) => {
+          return(
+          <ItemTile image={nft.asset.image} name={nft.asset.name} />
+        )})}
       </div>
       <Footer />
     </div>

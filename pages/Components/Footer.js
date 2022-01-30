8d66b675466582e6fad3be55ba6cdc8bd0e8ad70
Cyril Kunjumon <cyril 2019@iiitkottayam.ac.in>
2022-01-31 00:27:51 +0530

    footer

--- pages/Components/Footer.js
+++ pages/Components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
-    <div className="w-full bg-purple p-3 text-center font-extrabold">
-      Created with ❤️ by Cyril, Aryan and Charles
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 bg-gray mt-10 p-10 text-center justify-items-center font-medium">
+      <div className="logo"></div>
+      <div className="text-white">
+        <p>
+          Made with <span className="text-red">❤️</span> by Aryan, Charles and Cyril
+        </p>
+      </div>
     </div>
   );
 };
